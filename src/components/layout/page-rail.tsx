306634ba50a2e947a2b374a2f4a40a4010373b96
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-29 09:35:25 +0300

    Fix the watermark paint order and the page rail's x position

--- src/components/layout/page-rail.tsx
+++ src/components/layout/page-rail.tsx
@@ -7,7 +7,7 @@ export default function PageRail({ label }: PageRailProps) {
     <span
       aria-hidden="true"
       data-label={label}
-      className="absolute top-0 left-15.5 hidden text-body tracking-rail whitespace-nowrap text-light-grey uppercase [writing-mode:vertical-rl] before:content-[attr(data-label)] md:block lg:left-21.75"
+      className="absolute top-0 left-9.5 hidden text-body tracking-rail whitespace-nowrap text-light-grey uppercase [writing-mode:vertical-rl] before:content-[attr(data-label)] md:block lg:left-15.75"
     />
   );
 }
